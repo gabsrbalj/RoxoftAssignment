eb58f5ab9a3dad28db124e635565ec4f97ed458e
gabrijelasrbalj <gabrijela.srbalj@gmail.com>
2023-02-05 20:56:13 +0100

    Added TC13

---
+++ SauceTesting/SauceTesting/TC13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceTesting
+{
+    class TC13
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Console.Write("Website opened.\n");
+        }
+
+        [Test]
+
+        public void TestCase13()
+        {
+            CommonMethods commonMethods = new CommonMethods();
+
+            commonMethods.Login("standard_user", "secret_sauce");
+
+            commonMethods.SortByPriceHighToLow();
+
+            commonMethods.Logout();
+
+
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Properties.driver.Close();
+        }
+    }
+}
