d8a4780fdf6ffdce221fe49eeefc76e010df063e
gabrijelasrbalj <gabrijela.srbalj@gmail.com>
2023-02-05 21:09:47 +0100

    Added TC18

---
+++ SauceTesting/SauceTesting/TC18.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceTesting
+{
+    class TC18
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Console.Write("Website opened.\n");
+        }
+
+        [Test]
+
+        public void TestCase18()
+        {
+            CommonMethods commonMethods = new CommonMethods();
+            HomePageObject homeObj = new HomePageObject();
+            CartPageObject cartObj = new CartPageObject();
+            commonMethods.Login("standard_user", "secret_sauce");
+            commonMethods.VerifyUrl("https://www.saucedemo.com/inventory.html");
+
+            homeObj.cartIcon.Click();
+            cartObj.checkoutBtn.Click();
+
+            commonMethods.Checkout("", "Srbalj", "35254 Bebrina");
+            commonMethods.VerifyErrorMessage("Error: First Name is required");
+
+            commonMethods.Logout();
+
+
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Properties.driver.Close();
+        }
+    }
+}
