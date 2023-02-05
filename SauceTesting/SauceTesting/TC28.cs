783bbdd8301a1c648c59e617b0b14cae0602ef97
gabrijelasrbalj <gabrijela.srbalj@gmail.com>
2023-02-05 21:25:55 +0100

    Added not saved case

--- SauceTesting/SauceTesting/TC28.cs
+++ SauceTesting/SauceTesting/TC28.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SauceTesting
 {
     class TC28
     {
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Console.Write("Website opened.\n");
+        }
+
+        [Test]
+
+        public void TestCase28()
+        {
+            CommonMethods commonMethods = new CommonMethods();
+
+            commonMethods.Login("problem_user", "secret_sauce");
+
+            commonMethods.SortByPriceHighToLow();
+            Thread.Sleep(2000);
+
+            commonMethods.Logout();
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Properties.driver.Close();
+        }
     }
 }
