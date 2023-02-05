77c30c03dd0f50309735beed70d815ae57438f1b
gabrijelasrbalj <gabrijela.srbalj@gmail.com>
2023-02-05 21:17:00 +0100

    Added TC22 and TC23

---
+++ SauceTesting/SauceTesting/TC22.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceTesting
+{
+    class TC22
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Console.Write("Website opened.\n");
+        }
+
+        [Test]
+
+        public void TestCase22()
+        {
+            CommonMethods commonMethods = new CommonMethods();
+
+            commonMethods.Login("locked_out_user", "secret_sauce");
+
+            commonMethods.VerifyErrorMessage("Epic sadface: Sorry, this user has been locked out.");
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Properties.driver.Close();
+        }
+    }
+}
