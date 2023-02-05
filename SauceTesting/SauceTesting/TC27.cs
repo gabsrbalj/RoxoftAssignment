282a2e9c78aba49eccdb4a669751ab8d2ef647d2
gabrijelasrbalj <gabrijela.srbalj@gmail.com>
2023-02-05 21:20:26 +0100

    Added TC26 and TC27

---
+++ SauceTesting/SauceTesting/TC27.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SauceTesting
+{
+    class TC27
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Console.Write("Website opened.\n");
+        }
+
+        [Test]
+
+        public void TestCase27()
+        {
+            CommonMethods commonMethods = new CommonMethods();
+
+            commonMethods.Login("problem_user", "secret_sauce");
+
+            commonMethods.SortByPriceLowToHigh();
+            Thread.Sleep(2000);
+
+            commonMethods.Logout();
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Properties.driver.Close();
+        }
+    }
+}
