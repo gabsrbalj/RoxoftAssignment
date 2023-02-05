0ae25b2d4d0d140fc4025637c02a5606669da89a
gabrijelasrbalj <gabrijela.srbalj@gmail.com>
2023-02-05 20:53:16 +0100

    Added TC11

---
+++ SauceTesting/SauceTesting/TC11.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceTesting
+{
+    class TC11
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver();
+            Properties.driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Console.Write("Website opened.\n");
+        }
+
+        [Test]
+
+        public void TestCase11()
+        {
+            CommonMethods commonMethods = new CommonMethods();
+
+            commonMethods.Login("standard_user", "secret_sauce");
+
+            commonMethods.SortingItemsFromZToA();
+
+            commonMethods.Logout();
+
+
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Properties.driver.Close();
+        }
+    }
+}
